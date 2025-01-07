1ffeff4b25d6e02ccbc2884f0076566042e769e7
藤本大輝 <dahuit133@gmail.com>
2025-01-08 00:00:36 +0900

    Q2.多次元配列

--- php-practice_2.php
+++ php-practice_2.php
@@ -13,7 +13,34 @@ for ($i = 1; $i <= 100; $i++) {
 }
 
 // Q2 多次元連想配列
+$personalInfos = [
+  [
+      'name' => 'Aさん',
+      'mail' => 'aaa@mail.com',
+      'tel'  => '09011112222'
+  ],
+  [
+      'name' => 'Bさん',
+      'mail' => 'bbb@mail.com',
+      'tel'  => '08033334444'
+  ],
+  [
+      'name' => 'Cさん',
+      'mail' => 'ccc@mail.com',
+      'tel'  => '09055556666'
+  ],
+];
+echo $personalInfos[1]['name'] . 'の電話番号は' . $personalInfos[1]['tel'] . 'です。';
 
+foreach($personalInfos as $index => $element) {
+  echo ($index + 1) . '番目の' . $element['name'] . 'のメールアドレスは' . $element['mail'] . 'で、電話番号は' . $element['tel'] . 'です。' . "\n";
+}
+
+$ageList = [25, 30, 18];
+foreach($personalInfos as $index => &$element) {
+  $element['age'] = $ageList[$index];
+}
+var_dump($personalInfos);
 
 // Q3 オブジェクト-1
 
